7f8beb1eeb0f379159f1c5d2d74df65eff47b94d
klkirstyleeskl <k.lees@btinternet.com>
2019-03-11 16:40:06 +0000

    creates intial curve of Dugong data

---
+++ dugongs work.R
@@ -0,0 +1,2 @@
+attach(dugong)
+plot(x= Age, y=Length, xlab = "Age/years", ylab = "Length/m",main = "Maturation of Dugongs Michealis-Menten Curve", pch = 16, cex = 1, text(25,2.3, expression(y== frac(ax, 1+bx))))
